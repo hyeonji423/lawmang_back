a54e803fb2ce2f7569d1a5afe8d6c32ca70b3268
hyeonji423 <hyeon423423@naver.com>
2025-04-03 18:05:52 +0900

    location 변경 5

--- deploy.sh
+++ deploy.sh
@@ -73,10 +73,10 @@ server {
     location / {
         proxy_pass http://127.0.0.1:8000;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
 
 
         # ✅ OPTIONS 요청에만 CORS 헤더 적용
