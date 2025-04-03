8c1c45a668ecf0021e984f42d4cc8c48fd59da96
hyeonji423 <hyeon423423@naver.com>
2025-04-03 17:48:18 +0900

    location 변경 3

--- deploy.sh
+++ deploy.sh
@@ -78,21 +78,21 @@ server {
         proxy_set_header Host \$host;
         proxy_cache_bypass \$http_upgrade;
 
-        # CORS 설정 추가
-        add_header Access-Control-Allow-Origin "https://lawmang-front.vercel.app" always;
-        add_header Access-Control-Allow-Methods "GET, POST, PUT, DELETE, OPTIONS" always;
-        add_header Access-Control-Allow-Headers "Authorization, Content-Type, Accept, Origin, User-Agent" always;
-        add_header Access-Control-Allow-Credentials "true" always;
-
-        # OPTIONS 요청 처리
+        # CORS 설정
         if (\$request_method = "OPTIONS") {
-            add_header Access-Control-Allow-Origin "https://lawmang-front.vercel.app" always;
-            add_header Access-Control-Allow-Methods "GET, POST, PUT, DELETE, OPTIONS" always;
-            add_header Access-Control-Allow-Headers "Authorization, Content-Type, Accept, Origin, User-Agent" always;
-            add_header Content-Length 0;
-            add_header Content-Type text/plain;
+            add_header "Access-Control-Allow-Origin" "https://lawmang-front.vercel.app" always;
+            add_header "Access-Control-Allow-Methods" "GET, POST, PUT, DELETE, OPTIONS" always;
+            add_header "Access-Control-Allow-Headers" "Authorization, Content-Type, Accept, Origin, User-Agent" always;
+            add_header "Access-Control-Allow-Credentials" "true" always;
+            add_header "Content-Length" 0;
+            add_header "Content-Type" "text/plain charset=UTF-8";
             return 204;
         }
+
+        add_header "Access-Control-Allow-Origin" "https://lawmang-front.vercel.app" always;
+        add_header "Access-Control-Allow-Methods" "GET, POST, PUT, DELETE, OPTIONS" always;
+        add_header "Access-Control-Allow-Headers" "Authorization, Content-Type, Accept, Origin, User-Agent" always;
+        add_header "Access-Control-Allow-Credentials" "true" always;
     }
 }
 EOF'
