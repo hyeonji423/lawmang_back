7660944e1079e52349ea2812eea89601cb59bbd6
hyeonji423 <hyeon423423@naver.com>
2025-04-03 16:37:02 +0900

    vercel origin 변경

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(default_response_class=ORJSONResponse)
 # ✅ CORS 설정 (React와 연결할 경우 필수)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React 개발 서버 주소
+    allow_origins=["https://lawmang-front.vercel.app"],  # React 개발 서버 주소
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
